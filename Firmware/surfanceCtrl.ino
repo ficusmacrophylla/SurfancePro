6b9b61bfcffbad03475067356ff47d0d538fac5c
thetimevoyager <admin@basat.org>
2021-12-26 00:36:14 +0100

    Added manual mode to control the fan in a switch controlled mode.

--- Firmware/surfanceCtrl.ino
+++ Firmware/surfanceCtrl.ino
@@ -9,26 +9,28 @@
 //NOTE: I'm Assuming you are using the same fan model.
 
 #define PIN_SENSE 2 //where we connected the fan sense pin. Must be an interrupt capable pin (2 or 3 on Arduino Uno)
+#define PIN_BUTTON 3 //where we connected the button controller. Must be an interrupt capable pin.
 #define DEBOUNCE 0 //0 is fine for most fans, crappy fans may require 10 or 20 to filter out noise
 #define FANSTUCK_THRESHOLD 500 //if no interrupts were received for 500ms, consider the fan as stuck and report 0 RPM
 #define DELAYTIME 4000 //milliseconds to wait
 #define REDPIN 4
 #define GREENPIN 5
 #define BLUEPIN 6
+
 //Interrupt handler. Stores the timestamps of the last 2 interrupts and handles debouncing
 unsigned long volatile ts1=0,ts2=0;
+volatile int pressed = 0;
 
 //VARIABLES
+bool INPUTALLOWED = true;
 String inputFanSpeed, LightsCommand,delayS;
 float dutyVal;
-int delayT, i, input;
+int delayT, i, input, NbTopsFan, Calc;
 int redVal = 0, greenVal = 0, blueVal = 0;
 //This is the varible used to select the fan and it's divider,
     //set 1 for unipole hall effect sensor
     //and 2 for bipole hall effect sensor
 unsigned int fanDiv = 2;
-
-int NbTopsFan; int Calc;
 
 void tachISR() {
     unsigned long m=millis();
@@ -113,48 +115,104 @@ void SetLight(){
     analogWrite(BLUEPIN, blueVal);
 }
 
-void setFanSpeed(){
-    Serial.println("Set Fan Speed (10-100): ");
-    while (Serial.available() == 0)
-   /* just wait */ ;
+void setFanSpeed(int speed){
+    if(speed == -1)
+    { //manual mode
+        Serial.println("Manual Mode: ");
+        Serial.println("Set Fan Speed (10-100): ");
+        while (Serial.available() == 0)
+    /* just wait */ ;
 
-    /* read the incoming byte*/
- 
-    //delay(2000);
-    inputFanSpeed = Serial.readString();    
-    Serial.print("Readed: ");
-    Serial.println(inputFanSpeed);
+        /* read the incoming byte*/
+    
+        //delay(2000);
+        inputFanSpeed = Serial.readString();    
+        Serial.print("Readed: ");
+        Serial.println(inputFanSpeed);
 
-    Serial.print("Set Duration (number of 4sec. slots to try) ");
-    while (Serial.available() == 0)
-   /* just wait */ ;
+        Serial.print("Set Duration (number of 4sec. slots to try) ");
+        while (Serial.available() == 0)
+    /* just wait */ ;
 
-    /* read the incoming byte*/
- 
-    //delay(2000);
-    delayS = Serial.readString();    
-    Serial.print("Readed: ");
-    Serial.println(delayS.toInt());
-    delayT = delayS.toInt();
+        /* read the incoming byte*/
+    
+        //delay(2000);
+        delayS = Serial.readString();    
+        Serial.print("Readed: ");
+        Serial.println(delayS.toInt());
+        delayT = delayS.toInt();
 
-    dutyVal = convertToDutyCycle(inputFanSpeed.toInt());
-    Serial.print("Readed DutyVal: ");
-    Serial.println(dutyVal);
-    setPWM1A(dutyVal);
-    i=0;
-    while(i != delayT)
+        dutyVal = convertToDutyCycle(inputFanSpeed.toInt());
+        Serial.print("Readed DutyVal: ");
+        Serial.println(dutyVal);
+        setPWM1A(dutyVal);
+        i=0;
+        while(i != delayT)
+        {
+            
+            CalcRpm(); //reads RPM
+            
+
+            i++;
+        }
+    }else //auto mode
     {
+        dutyVal = convertToDutyCycle(speed);
+        Serial.print("Readed DutyVal: ");
+        Serial.println(dutyVal);
+        setPWM1A(dutyVal);
         
-        CalcRpm(); //reads RPM
-        
-
-        i++;
     }
 }
 
 void rpmCount ()
 //This is the function that the interupt calls
-{ NbTopsFan++; }
+{ 
+    NbTopsFan++; 
+}
+
+void buttonPressed(){
+    //se token occupato non faccio nulla
+    if(!INPUTALLOWED){
+        Serial.println("BUTTON DISABLED!");
+    }else {
+    //detachInterrupt(buttonPressed);
+    INPUTALLOWED = false;
+    //noInterrupts();
+    pressed++;
+    Serial.println("BUTTON PRESSED!");
+
+    if(pressed > 4)
+        pressed = 0;
+    switch (pressed)
+    {
+    case 0:
+        setFanSpeed(0);
+        //Serial.println("Speed set at: " + 25*pressed);
+        break;
+    case 1:
+        setFanSpeed(25);
+        //Serial.println("Speed set at: " + 25*pressed);
+        break;
+    case 2:
+        setFanSpeed(50);
+        //Serial.println("Speed set at: " + 25*pressed);
+        break;
+    case 3:
+        setFanSpeed(75);
+        //Serial.println("Speed set at: " + 25*pressed);
+        break;
+    case 4:
+        setFanSpeed(100);
+        //Serial.println("Speed set at: " + 25*pressed);
+        break;
+    default:
+        setFanSpeed(0);
+        Serial.println("Wrong number insertion: " + pressed);
+        break;
+    }
+    }
+}
 
 void setup(){
     //enable outputs for Timer 1
@@ -171,28 +229,37 @@ void setup(){
     //note that pin 11 will be unavailable for output in this mode!
 
     pinMode(PIN_SENSE,INPUT_PULLUP); //set the sense pin as input with pullup resistor
+    pinMode(PIN_BUTTON,INPUT_PULLUP); //set the button pin as input with pull
    // attachInterrupt(digitalPinToInterrupt(PIN_SENSE),tachISR,FALLING); //set tachISR to be triggered when the signal on the sense pin goes low
     attachInterrupt(digitalPinToInterrupt(PIN_SENSE),rpmCount,RISING); //set tachISR to be triggered when the signal on the sense pin goes low
+    attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), buttonPressed, LOW); // trigger when button pressed, but not when released.
     Serial.begin(9600); //enable serial so we can see the RPM in the serial monitor
     //digitalWrite(5,HIGH); //spengo
     
     
 
 
+
     
         
 }
-void loop(){   
-    Serial.println("1 : Fan Speed - 2 : Light Color \n");
-    while (Serial.available() == 0)
-   /* just wait */ ;
-   input = Serial.readString().toInt();
-   if(input == 1)
-        setFanSpeed();
-   else if(input == 2)
-        SetLight();
-    else;
-    //delay(DELAYTIME);
+void loop(){  
+    //Serial.println("Fan Standing By, awaiting command...");
+    delay(2000);
+    INPUTALLOWED = true;
+    //attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), buttonPressed, LOW); // trigger when button pressed, but not when released.
+    //interrupts();
+
+// Serial.println("1 : Fan Speed - 2 : Light Color \n");
+//     while (Serial.available() == 0)
+//    /* just wait */ ;
+//    input = Serial.readString().toInt();
+//    if(input == 1)
+//         setFanSpeed();
+//    else if(input == 2)
+//         SetLight();
+//     else;
+//     //delay(DELAYTIME);
     
 
 
